348a11af282e08d306b01b0471d19703ec54dee9
Amansingh9931 <amansingh12073@gmail.com>
2025-11-21 20:45:56 +0530

    WIP: updates to frontend files

--- Frontend/src/components/user/Hero.jsx
+++ Frontend/src/components/user/Hero.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <section className="relative w-full min-h-[90vh] flex items-center px-6 md:px-12">
       {/* Background gradient very similar to Modvel */}
-      <div className="absolute inset-0 bg-gradient-to-br from-green-50 via-green-100 to-white pointer-events-none" />
+      <div className="absolute inset-0 bg-linear-to-br from-green-50 via-green-100 to-white pointer-events-none" />
 
       <div className="relative max-w-7xl w-full grid grid-cols-1 md:grid-cols-2 items-center gap-8">
         {/* Left Text */}
@@ -44,16 +44,16 @@ const HeroSection = () => {
         </motion.div>
 
         {/* Right Image */}
- <motion.div
-  initial={{ opacity: 0, x: 250 }}   // farther RIGHT
-  animate={{ opacity: 1, x: 0 }}     // slide to center
-  transition={{ duration: 1.2, ease: "easeOut" }}
-  className="flex justify-end"
->
-  <motion.img
-    src={assets.girl}
-    alt="Hero Girl"
-    className="
+        <motion.div
+          initial={{ opacity: 0, x: 250 }} // farther RIGHT
+          animate={{ opacity: 1, x: 0 }} // slide to center
+          transition={{ duration: 1.2, ease: "easeOut" }}
+          className="flex justify-end"
+        >
+          <motion.img
+            src={assets.girl}
+            alt="Hero Girl"
+            className="
       w-[1800px] md:w-[1400px] lg:w-[1600px]
       object-contain
       drop-shadow-2xl
@@ -62,10 +62,9 @@ const HeroSection = () => {
       select-none
       transition-all duration-500
     "
-    whileHover={{ scale: 1.06 }}
-  />
-</motion.div>
-
+            whileHover={{ scale: 1.06 }}
+          />
+        </motion.div>
       </div>
     </section>
   );
